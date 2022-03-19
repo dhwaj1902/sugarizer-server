89e7f6daf77bc0c70936e2e280e39ae9628ccb82
Dhwaj Gupta <57860123+dhwaj1902@users.noreply.github.com>
2022-03-19 15:26:38 +0530

    fix all eslint errors

--- api/controller/utils/qrCodeUtil.js
+++ api/controller/utils/qrCodeUtil.js
@@ -1,7 +1,6 @@
 var qrcode = require('qrcode');
 
 exports.generateQRCode = async function (otpAuth) {
-
     try {
         var QRCodeImageUrl = await qrcode.toDataURL(otpAuth);
         return { image: `<img src='${QRCodeImageUrl}' alt='qr-code-img' />` };
